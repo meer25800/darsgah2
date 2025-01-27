f64fc9f2b6cf7776ba55f68377644463c4f3f714
Aamir Hameed mir <30907975+meer25800@users.noreply.github.com>
2025-01-27 20:25:44 +0530

    Update student.py

--- PythonProject1/student.py
+++ PythonProject1/student.py
@@ -2,13 +2,18 @@ import streamlit as st
 import pandas as pd
 from PIL import Image
 import base64
+import os
 def get_image_base64(image_path):
     with open(image_path, "rb") as img_file:
         encoded = base64.b64encode(img_file.read()).decode("utf-8")
     return encoded
 
 # Path to the logo
+
+
 logo_path = "Screenshot__193_-removebg-preview.png"
+if not os.path.exists(logo_path):
+    print(f"File not found: {logo_path}")
 
 # Get base64 string of the image
 logo_base64 = get_image_base64(logo_path)
