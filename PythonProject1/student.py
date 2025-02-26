d7ea0d638c1e4223adef08204d2784b53e0235f5
Aamir Hameed mir <30907975+meer25800@users.noreply.github.com>
2025-02-26 22:21:39 +0530

    Update student.py

--- PythonProject1/student.py
+++ PythonProject1/student.py
@@ -476,10 +476,10 @@ elif choice == "Syllabus":
     syllabus_images = {
         "Class 1st": "https://raw.githubusercontent.com/meer25800/darsgah2/main/PythonProject1/Screenshot__193_-removebg-preview.png",
         "Class 2nd": "https://raw.githubusercontent.com/meer25800/darsgah2/main/PythonProject1/Screenshot__193_-removebg-preview.png",
-        "Class 3rd": "https://raw.githubusercontent.com/meer25800/darsgah2/blob/main/PythonProject1/a-3.png",
+        "Class 3rd": "https://raw.githubusercontent.com/meer25800/darsgah2/main/PythonProject1/a-3.png",
         "Class 4th": "https://raw.githubusercontent.com/meer25800/darsgah2/main/PythonProject1/a-3.png",
-        "Class 5th": "https://raw.githubusercontent.com/meer25800/darsgah2/main/PythonProject1/Screenshot__193_-removebg-preview.png",
-        "Class 6th": "https://raw.githubusercontent.com/meer25800/darsgah2/main/PythonProject1/Screenshot__193_-removebg-preview.png",
+        "Class 5th": "https://raw.githubusercontent.com/meer25800/darsgah2/main/PythonProject1/5th Syllabus-1.png",
+        "Class 6th": "https://raw.githubusercontent.com/meer25800/darsgah2/main/PythonProject1/6th Syllabus-1.png",
         "Class 7th": "https://raw.githubusercontent.com/meer25800/darsgah2/main/PythonProject1/Screenshot__193_-removebg-preview.png",
         "Class 8th": "https://raw.githubusercontent.com/meer25800/darsgah2/main/PythonProject1/Screenshot__193_-removebg-preview.png",
     }
