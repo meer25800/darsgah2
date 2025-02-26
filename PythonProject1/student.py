c1bc175482d36a60b2f85d4bf93879e4ac5d066b
Aamir Hameed mir <30907975+meer25800@users.noreply.github.com>
2025-02-26 19:38:58 +0530

    Update student.py

--- PythonProject1/student.py
+++ PythonProject1/student.py
@@ -28,7 +28,7 @@ st.markdown("""
     <style>
         /* Change background color to white */
         .stApp {
-           background-color: #25be75
+           background-color: #6525be
 
         }
     </style>
